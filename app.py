0bb55125738b9f9f46cb05ffba4b1c982f07f79f
ditinagrawal <ditin.agrawal05@gmail.com>
2024-11-10 13:27:54 +0530

    LexPilot

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("LexPilot")
-    st.header("Chat with PDF")
+    st.header("LexPilot")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
